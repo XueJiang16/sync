f6ac767fb9572f0db0376fd5251c8c56d7d47bf5
jx <763565607@qq.com>
2022-06-29 21:43:37 +0800

    add

--- mmclassification_dev/mmcls/models/ood/gradnorm.py
+++ mmclassification_dev/mmcls/models/ood/gradnorm.py
@@ -20,7 +20,7 @@ class GradNorm(BaseModule):
         self.classifier.zero_grad()
         img = input['img']
         assert img.shape[0] == 1, "GradNorm backward implementation only supports batch = 1."
-        outputs = self.classifier.simple_test(softmax=False, **input)
+        outputs = self.classifier.simple_test(softmax=False, post_process=False, **input)
         # outputs, _ = self.classifier.simple_test(softmax=False, **input)
         targets = torch.ones((img.shape[0], self.num_classes)).cuda()
         outputs = outputs / self.temperature
