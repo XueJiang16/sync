a560c420e495dba960267c2065437b75380ea611
jx <763565607@qq.com>
2022-06-29 22:31:10 +0800

    add

--- mmclassification_dev/tools/ood_test.py
+++ mmclassification_dev/tools/ood_test.py
@@ -127,6 +127,8 @@ def main():
         data_loader_ood.append(build_dataloader(ood_set, **test_loader_cfg))
 
     model = build_ood_model(cfg.model)
+    print(cfg.gpu_ids)
+    assert False
     model = MMDataParallel(model, device_ids=cfg.gpu_ids)
 
     outputs_id = single_gpu_test_ood(model, data_loader_id)
