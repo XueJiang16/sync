8ad40b3085dafbc1bf53b3bc76cc5ffbd37b1f06
jx <763565607@qq.com>
2022-06-29 21:53:32 +0800

    add

--- mmclassification_dev/custom_config/resnet50.py
+++ mmclassification_dev/custom_config/resnet50.py
@@ -25,25 +25,10 @@ model = dict(
 data = dict(
     samples_per_gpu=1,
     workers_per_gpu=1,
-    # id_data=dict(
-    #     type='TxtDataset',
-    #     path='/data/csxjiang/val',
-    #     data_ann='/data/csxjiang/meta/val_labeled.txt',
-    #     pipeline=[
-    #         dict(type='LoadImageFromFile'),
-    #         dict(type='Resize', size=480),
-    #         dict(
-    #             type='Normalize',
-    #             mean=[123.675, 116.28, 103.53],
-    #             std=[58.395, 57.12, 57.375],
-    #             to_rgb=True),
-    #         dict(type='ImageToTensor', keys=['img']),
-    #         dict(type='Collect', keys=['img'])
-    #     ]),
     id_data=dict(
-        type='JsonDataset',
-        path='/data/csxjiang/',
-        data_ann='/data/csxjiang/ood_data/inat/val2018.json',
+        type='TxtDataset',
+        path='/data/csxjiang/val',
+        data_ann='/data/csxjiang/meta/val_labeled.txt',
         pipeline=[
             dict(type='LoadImageFromFile'),
             dict(type='Resize', size=480),
@@ -55,6 +40,21 @@ data = dict(
             dict(type='ImageToTensor', keys=['img']),
             dict(type='Collect', keys=['img'])
         ]),
+    # id_data=dict(
+    #     type='JsonDataset',
+    #     path='/data/csxjiang/',
+    #     data_ann='/data/csxjiang/ood_data/inat/val2018.json',
+    #     pipeline=[
+    #         dict(type='LoadImageFromFile'),
+    #         dict(type='Resize', size=480),
+    #         dict(
+    #             type='Normalize',
+    #             mean=[123.675, 116.28, 103.53],
+    #             std=[58.395, 57.12, 57.375],
+    #             to_rgb=True),
+    #         dict(type='ImageToTensor', keys=['img']),
+    #         dict(type='Collect', keys=['img'])
+    #     ]),
     ood_data=[],
     # ood_data=dict(
     #     type='TxtDataset',
