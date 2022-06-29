6648f0c98b3c51115f783b4bf283a36fa3a02b4a
jx <763565607@qq.com>
2022-06-29 21:19:41 +0800

    add

--- mmclassification_dev/mmcls/models/ood/gradnorm.py
+++ mmclassification_dev/mmcls/models/ood/gradnorm.py
@@ -7,13 +7,14 @@ from mmcls.models import build_classifier
 
 @OOD.register_module()
 class GradNorm(BaseModule):
-    def __init__(self, classifier, num_classes, temperature):
+    def __init__(self, classifier, num_classes, temperature, **kwargs):
         super(GradNorm, self).__init__()
         self.classifier = build_classifier(classifier)
         self.classifier.eval()
         self.num_classes = num_classes
         self.temperature = temperature
         self.logsoftmax = torch.nn.LogSoftmax(dim=-1).cuda()
+        print(kwargs)
 
     def simple_test(self, img, img_metas=None, **kwargs):
         self.classifier.zero_grad()
