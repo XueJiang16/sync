2ce8370481fc0529943cdee0c620a97ac3809e0c
jx <763565607@qq.com>
2022-06-29 21:17:02 +0800

    add

--- mmclassification_dev/mmcls/models/__init__.py
+++ mmclassification_dev/mmcls/models/__init__.py
@@ -7,6 +7,7 @@ from .classifiers import *  # noqa: F401,F403
 from .heads import *  # noqa: F401,F403
 from .losses import *  # noqa: F401,F403
 from .necks import *  # noqa: F401,F403
+from .ood import *
 
 __all__ = [
     'BACKBONES', 'HEADS', 'NECKS', 'LOSSES', 'CLASSIFIERS', 'build_backbone',
