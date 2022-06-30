43f0c6461bfc91ca9e5464f80b47c5e3f1688277
jx <763565607@qq.com>
2022-06-30 23:28:13 +0800

    add

--- mmclassification_dev/mmcls/datasets/ood_dataset.py
+++ mmclassification_dev/mmcls/datasets/ood_dataset.py
@@ -31,8 +31,8 @@ class OODBaseDataset(Dataset):
             self.data_infos.append(info)
 
     def __len__(self):
-        return 100
-        # return len(self.file_list)
+        # return 100
+        return len(self.file_list)
 
     def prepare_data(self, idx):
         results = copy.deepcopy(self.data_infos[idx])
