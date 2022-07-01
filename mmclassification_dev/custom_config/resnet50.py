4ef4977ff790bd92b4c5bb2775dbfb12713073fa
jx <763565607@qq.com>
2022-07-01 10:16:13 +0800

    add

--- mmclassification_dev/custom_config/resnet50.py
+++ mmclassification_dev/custom_config/resnet50.py
@@ -30,7 +30,7 @@ pipline =[
               type='Normalize',
               mean=[123.675, 116.28, 103.53],
               std=[58.395, 57.12, 57.375],
-              to_rgb=False),
+              to_rgb=True),
           dict(type='ImageToTensor', keys=['img']),
           dict(type='Collect', keys=['img'])
 ]
