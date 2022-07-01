4608e7491ad3c367acd5442399f0bedbbd914ed6
jx <763565607@qq.com>
2022-07-01 12:18:07 +0800

    add

--- mmclassification_dev/mmcls/models/ood/gradnorm.py
+++ mmclassification_dev/mmcls/models/ood/gradnorm.py
@@ -1,6 +1,8 @@
 from mmcv.runner import BaseModule
 import torch
 import os
+import numpy as np
+from collections import Counter
 
 from ..builder import OOD
 from mmcls.models import build_classifier
@@ -8,7 +10,7 @@ from mmcls.models import build_classifier
 
 @OOD.register_module()
 class GradNorm(BaseModule):
-    def __init__(self, classifier, num_classes, temperature, **kwargs):
+    def __init__(self, classifier, num_classes, temperature, target_file=None, **kwargs):
         super(GradNorm, self).__init__()
         self.local_rank = os.environ['LOCAL_RANK']
         self.classifier = build_classifier(classifier)
@@ -16,6 +18,22 @@ class GradNorm(BaseModule):
         self.num_classes = num_classes
         self.temperature = temperature
         self.logsoftmax = torch.nn.LogSoftmax(dim=-1).to("cuda:{}".format(self.local_rank))
+        if target_file is not None:
+            cls_idx = []
+            with open(target_file, 'r') as f:
+                for line in f.readlines():
+                    segs = line.strip().split(' ')
+                    cls_idx.append(int(segs[-1]))
+            cls_idx = np.array(cls_idx, dtype='int')
+            label_stat = Counter(cls_idx)
+            cls_num = [-1 for _ in range(num_classes)]
+            for i in range(num_classes):
+                cat_num = int(label_stat[i])
+                cls_num[i] = cat_num
+            target = cls_num / np.sum(cls_num)
+            self.target = torch.tensor(target).to("cuda:{}".format(self.local_rank)).unsqueeze(0)
+        else:
+            self.target = torch.ones((1, self.num_classes)).to("cuda:{}".format(self.local_rank))
 
     def forward(self, **input):
         self.classifier.zero_grad()
@@ -25,7 +43,7 @@ class GradNorm(BaseModule):
         # print("Self rank: {}, output device = {}".format(self.local_rank, outputs.device))
         # assert False
         # outputs, _ = self.classifier.simple_test(softmax=False, **input)
-        targets = torch.ones((img.shape[0], self.num_classes)).to("cuda:{}".format(self.local_rank))
+        targets = self.target
         outputs = outputs / self.temperature
         loss = torch.sum(torch.mean(-targets * self.logsoftmax(outputs), dim=-1))
 
@@ -36,7 +54,7 @@ class GradNorm(BaseModule):
 
 @OOD.register_module()
 class GradNormBatch(BaseModule):
-    def __init__(self, classifier, num_classes, temperature, **kwargs):
+    def __init__(self, classifier, num_classes, temperature, target_file=None,**kwargs):
         super(GradNormBatch, self).__init__()
         self.local_rank = os.environ['LOCAL_RANK']
         classifier['head']['require_features'] = True
@@ -44,14 +62,31 @@ class GradNormBatch(BaseModule):
         self.classifier.eval()
         self.num_classes = num_classes
         self.temperature = temperature
+        if target_file is not None:
+            cls_idx = []
+            with open(target_file, 'r') as f:
+                for line in f.readlines():
+                    segs = line.strip().split(' ')
+                    cls_idx.append(int(segs[-1]))
+            cls_idx = np.array(cls_idx, dtype='int')
+            label_stat = Counter(cls_idx)
+            cls_num = [-1 for _ in range(num_classes)]
+            for i in range(num_classes):
+                cat_num = int(label_stat[i])
+                cls_num[i] = cat_num
+            target = cls_num / np.sum(cls_num)
+            self.target = torch.tensor(target).to("cuda:{}".format(self.local_rank)).unsqueeze(0)
+        else:
+            self.target = torch.ones((1, self.num_classes)).to("cuda:{}".format(self.local_rank)) / self.num_classes
 
     def forward(self, **input):
         with torch.no_grad():
             outputs, features = self.classifier(return_loss=False, softmax=False, post_process=False, **input)
             U = torch.norm(features, p=1, dim=1)
             out_softmax = torch.nn.functional.softmax(outputs, dim=1)
-            V = torch.norm((1 - self.num_classes * out_softmax), p=1, dim=1)
-            S = U * V / 2048 / self.num_classes
+            targets = self.target
+            V = torch.norm((targets - out_softmax), p=1, dim=1)
+            S = U * V / 2048
         return S
 
 
