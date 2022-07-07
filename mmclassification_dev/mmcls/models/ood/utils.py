a8f6be49ba8b02c645a720e6ccef8859dd2c9f69
1@whughw.com <ghw970403>
2022-07-07 18:26:59 +0800

    ghw push

--- mmclassification_dev/mmcls/models/ood/utils.py
+++ mmclassification_dev/mmcls/models/ood/utils.py
@@ -8,7 +8,7 @@ def print_category(outputs, softmax=False):
     pred = outputs.detach().clone()
     if softmax:
         pred = F.softmax(pred, dim=1)
+    pred_cat = torch.argmax(pred, dim=1).cpu().tolist()
     if os.environ['LOCAL_RANK'] == '0':
-        print(torch.argmax(pred, dim=1))
-
+        print(pred_cat)
 
