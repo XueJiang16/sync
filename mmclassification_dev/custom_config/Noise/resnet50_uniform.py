adc91c44ba65d4b7643601d05ea81f0429a76f40
1@whughw.com <ghw970403>
2022-07-07 23:07:00 +0800

    ghw push

--- mmclassification_dev/custom_config/Noise/resnet50_uniform.py
+++ mmclassification_dev/custom_config/Noise/resnet50_uniform.py
@@ -1,7 +1,7 @@
 method_name = 'GradNormBatchScore'
 model_name = 'resnet50'
 train_dataset = 'LT_a8'
-custom_name = "NoiseUniform_0_127"
+custom_name = "NoiseUniform_128_255"
 if custom_name is not None:
     readable_name = '{}_{}_{}_{}'.format(method_name, model_name, train_dataset, custom_name)
 else:
