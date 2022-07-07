271742f5fa167c78648f84bb0387ebbd9f748c53
1@whughw.com <ghw970403>
2022-07-07 18:52:03 +0800

    [New Feature] Debug - print_topk

--- mmclassification_dev/mmcls/models/ood/gradnorm.py
+++ mmclassification_dev/mmcls/models/ood/gradnorm.py
@@ -6,7 +6,7 @@ from collections import Counter
 
 from ..builder import OOD
 from mmcls.models import build_classifier
-from .utils import print_category
+from .utils import print_category, print_topk
 
 
 @OOD.register_module()
@@ -88,7 +88,7 @@ class GradNormBatch(BaseModule):
         with torch.no_grad():
             outputs, features = self.classifier(return_loss=False, softmax=False, post_process=False, **input)
             if self.debug_mode:
-                print_category(outputs, softmax=True)
+                print_topk(outputs, softmax=True)
             U = torch.norm(features, p=1, dim=1)
             out_softmax = torch.nn.functional.softmax(outputs, dim=1)
             targets = self.target
--- mmclassification_dev/mmcls/models/ood/utils.py
+++ mmclassification_dev/mmcls/models/ood/utils.py
@@ -12,3 +12,10 @@ def print_category(outputs, softmax=False):
     if os.environ['LOCAL_RANK'] == '0':
         print(pred_cat)
 
+def print_topk(outputs, softmax=False, k=3):
+    pred = outputs.detach().clone()
+    if softmax:
+        pred = F.softmax(pred, dim=1)
+    _, pred_cat = torch.topk(pred, k, dim=1).cpu().tolist()
+    if os.environ['LOCAL_RANK'] == '0':
+        print(pred_cat)
