b41bf7a33227c1865228e7c11d23e407107ee310
jx <763565607@qq.com>
2022-07-08 22:54:00 +0800

    jx commit

--- mmclassification_dev/custom_config/resnet50.py
+++ mmclassification_dev/custom_config/resnet50.py
@@ -1,4 +1,4 @@
-method_name = 'GradNormCos'
+method_name = 'GradNormCos_inverse'
 model_name = 'resnet50'
 train_dataset = 'LT_a8'
 readable_name ='{}_{}_{}'.format(method_name, model_name, train_dataset)
