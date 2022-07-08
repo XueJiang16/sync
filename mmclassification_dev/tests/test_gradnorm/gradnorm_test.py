ac5916bcddec7dfe97e47c89748a0732b79d81ef
1@whughw.com <ghw970403>
2022-07-08 21:26:45 +0800

    [New Feature] gradnorm_test

--- mmclassification_dev/tests/test_gradnorm/gradnorm_test.py
+++ mmclassification_dev/tests/test_gradnorm/gradnorm_test.py
@@ -4,9 +4,8 @@ import matplotlib.pyplot as plt
 
 outputs = torch.rand(10000, 1000)
 logsoftmax = torch.nn.LogSoftmax(dim=-1)
-target = torch.ones((10000, 1000))
-loss = torch.mean(-target * logsoftmax(outputs), dim=-1).numpy()
+loss = torch.mean(-logsoftmax(outputs), dim=-1).numpy()
 out_softmax = torch.nn.functional.softmax(outputs, dim=1)
-V = torch.norm((target - out_softmax), p=1, dim=1).numpy()
-plt.plot(loss, V)
+V = torch.norm((out_softmax), p=1, dim=1).numpy()
+plt.scatter(loss, V)
 plt.savefig("gradnorm_loss_gradient.pdf")
