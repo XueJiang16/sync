59374aff80e2749dfe7868a8bded17387084124c
jx <763565607@qq.com>
2022-07-11 16:53:40 +0800

    Energy update

--- mmclassification_dev/mmcls/models/ood/__init__.py
+++ mmclassification_dev/mmcls/models/ood/__init__.py
@@ -1,8 +1,9 @@
 from .gradnorm import GradNorm, GradNormBatch, GradNormBatchScore, GradNormCos
 from .msp import MSP, MSPCustom
 from .odin import ODIN, ODINCustom
+from .energy import Energy, EnergyCustom
 
 __all__ = [
     'GradNorm', 'GradNormBatch', 'GradNormBatchScore', 'MSP', 'MSPCustom',
-    'GradNormCos', 'ODIN', 'ODINCustom'
+    'GradNormCos', 'ODIN', 'ODINCustom', 'Energy', 'EnergyCustom'
 ]
