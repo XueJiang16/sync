4b2ed2bc247e9e3cca5bd0adc8c0dcbe26fadc64
1@whughw.com <ghw970403>
2022-07-13 12:12:52 +0800

    crop mean

--- mmclassification_dev/mmcls/apis/test_ood.py
+++ mmclassification_dev/mmcls/apis/test_ood.py
@@ -101,8 +101,8 @@ def ssim_test(img, img_metas=None, **kwargs):
     crops_mean.sort()
     crops_std.sort()
     num_crops = len(crops_mean)
-    lower_percentile = 0.1
-    upper_percentile = 1.0
+    lower_percentile = 0.05
+    upper_percentile = 0.95
     lower_bound = int(lower_percentile * num_crops)
     upper_bound = int(upper_percentile * num_crops)
     crops_mean = crops_mean[lower_bound: upper_bound]
