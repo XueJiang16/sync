3e37d3cdb5783e1f2413f9e6d17640320b1fc867
jx <763565607@qq.com>
2022-07-13 16:56:23 +0800

    mean std test

--- mmclassification_dev/custom_config/ssim.py
+++ mmclassification_dev/custom_config/ssim.py
@@ -1,7 +1,7 @@
 method_name = 'GradNormBatch'
 model_name = 'resnet50'
 train_dataset = 'LT_a8'
-custom_name = 'mean_std_places_textures'
+custom_name = 'mean_std_imagenet_all'
 if custom_name is not None:
     readable_name = '{}_{}_{}_{}'.format(method_name, model_name, train_dataset, custom_name)
 else:
