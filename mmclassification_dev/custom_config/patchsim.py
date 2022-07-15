295ad2e70b1fa62801ebddf1fca601aace400169
jx <763565607@qq.com>
2022-07-15 18:20:43 +0800

    jx commit

--- mmclassification_dev/custom_config/patchsim.py
+++ mmclassification_dev/custom_config/patchsim.py
@@ -1,7 +1,7 @@
 method_name = 'PatchSim'
 model_name = 'resnet50'
 train_dataset = 'LT_a8'
-custom_name = 'GradNormBatch_0.5order'
+custom_name = 'GradNormBatch_2order'
 if custom_name is not None:
     readable_name = '{}_{}_{}_{}'.format(method_name, model_name, train_dataset, custom_name)
 else:
