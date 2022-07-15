3da57d17ba7e2148e7a2a9db63c0a0015fd6fe8a
jx <763565607@qq.com>
2022-07-15 23:31:15 +0800

    jx commit

--- mmclassification_dev/mmcls/models/ood/feature_level.py
+++ mmclassification_dev/mmcls/models/ood/feature_level.py
@@ -63,12 +63,13 @@ class PatchSim(BaseModule):
                     count += 1
             patch_sim /= count
             patch_sim = (patch_sim + 1) / 2
-            if self.has_ood_detector:
-                ood_scores, _ = self.ood_detector(**input)
-                patch_sim = ((1 / self.threshold) ** (self.order)) * torch.pow(patch_sim, self.order)
-                patch_sim[patch_sim > 1] = 1
-                ood_scores *= patch_sim
-            else:
-                ood_scores = patch_sim
+            ood_scores = patch_sim
+            # if self.has_ood_detector:
+            #     ood_scores, _ = self.ood_detector(**input)
+            #     patch_sim = ((1 / self.threshold) ** (self.order)) * torch.pow(patch_sim, self.order)
+            #     patch_sim[patch_sim > 1] = 1
+            #     ood_scores *= patch_sim
+            # else:
+            #     ood_scores = patch_sim
         return ood_scores, type
 
