d8f8e2a7d3db111eac0a3ea7d23f4f087febbfb4
jx <763565607@qq.com>
2022-07-17 22:38:12 +0800

    jx commit

--- mmclassification_dev/mmcls/utils/ood_evaluate.py
+++ mmclassification_dev/mmcls/utils/ood_evaluate.py
@@ -75,101 +75,101 @@ def get_measures(in_examples, out_examples):
     aupr_out = sk.average_precision_score(labels_rev, examples)
     return auroc, aupr_in, aupr_out, fpr
 
-# def evaluate_all(in_scores, out_scores):
-#     in_examples = in_scores.reshape((-1, 1))
-#     out_examples = out_scores.reshape((-1, 1))
-#     auroc, aupr_in, aupr_out, fpr95 = get_measures(in_examples, out_examples)
-#     return auroc, aupr_in, aupr_out, fpr95
-
-# from knn-ood github
-def cal_metric(known, novel, method=None):
-    tp, fp, fpr_at_tpr95 = get_curve(known, novel, method)
-    results = dict()
-    # FPR
-    mtype = 'FPR'
-    results[mtype] = fpr_at_tpr95
-
-    # AUROC
-    mtype = 'AUROC'
-    tpr = np.concatenate([[1.], tp/tp[0], [0.]])
-    fpr = np.concatenate([[1.], fp/fp[0], [0.]])
-    results[mtype] = -np.trapz(1.-fpr, tpr)
-
-    # AUIN
-    mtype = 'AUIN'
-    denom = tp+fp
-    denom[denom == 0.] = -1.
-    pin_ind = np.concatenate([[True], denom > 0., [True]])
-    pin = np.concatenate([[.5], tp/denom, [0.]])
-    results[mtype] = -np.trapz(pin[pin_ind], tpr[pin_ind])
-
-    # AUOUT
-    mtype = 'AUOUT'
-    denom = tp[0]-tp+fp[0]-fp
-    denom[denom == 0.] = -1.
-    pout_ind = np.concatenate([[True], denom > 0., [True]])
-    pout = np.concatenate([[0.], (fp[0]-fp)/denom, [.5]])
-    results[mtype] = np.trapz(pout[pout_ind], 1.-fpr[pout_ind])
-
-    return results['AUROC'], results['AUIN'], results['AUOUT'], results['FPR']
-
-def get_curve(known, novel, method=None):
-    tp, fp = dict(), dict()
-    fpr_at_tpr95 = dict()
-
-    known.sort()
-    novel.sort()
-
-    end = np.max([np.max(known), np.max(novel)])
-    start = np.min([np.min(known),np.min(novel)])
-
-    all = np.concatenate((known, novel))
-    all.sort()
-
-    num_k = known.shape[0]
-    num_n = novel.shape[0]
-
-    if method == 'row':
-        threshold = -0.5
-    else:
-        threshold = known[round(0.05 * num_k)]
-
-    tp = -np.ones([num_k+num_n+1], dtype=int)
-    fp = -np.ones([num_k+num_n+1], dtype=int)
-    tp[0], fp[0] = num_k, num_n
-    k, n = 0, 0
-    for l in range(num_k+num_n):
-        if k == num_k:
-            tp[l+1:] = tp[l]
-            fp[l+1:] = np.arange(fp[l]-1, -1, -1)
-            break
-        elif n == num_n:
-            tp[l+1:] = np.arange(tp[l]-1, -1, -1)
-            fp[l+1:] = fp[l]
-            break
-        else:
-            if novel[n] < known[k]:
-                n += 1
-                tp[l+1] = tp[l]
-                fp[l+1] = fp[l] - 1
-            else:
-                k += 1
-                tp[l+1] = tp[l] - 1
-                fp[l+1] = fp[l]
-
-    j = num_k+num_n-1
-    for l in range(num_k+num_n-1):
-        if all[j] == all[j-1]:
-            tp[j] = tp[j+1]
-            fp[j] = fp[j+1]
-        j -= 1
-
-    fpr_at_tpr95 = np.sum(novel > threshold) / float(num_n)
-
-    return tp, fp, fpr_at_tpr95
-
 def evaluate_all(in_scores, out_scores):
-    in_examples = in_scores.reshape((-1,))
-    out_examples = out_scores.reshape((-1,))
-    auroc, aupr_in, aupr_out, fpr95 = cal_metric(in_examples, out_examples)
+    in_examples = in_scores.reshape((-1, 1))
+    out_examples = out_scores.reshape((-1, 1))
+    auroc, aupr_in, aupr_out, fpr95 = get_measures(in_examples, out_examples)
     return auroc, aupr_in, aupr_out, fpr95
+
+## from knn-ood github
+# def cal_metric(known, novel, method=None):
+#     tp, fp, fpr_at_tpr95 = get_curve(known, novel, method)
+#     results = dict()
+#     # FPR
+#     mtype = 'FPR'
+#     results[mtype] = fpr_at_tpr95
+#
+#     # AUROC
+#     mtype = 'AUROC'
+#     tpr = np.concatenate([[1.], tp/tp[0], [0.]])
+#     fpr = np.concatenate([[1.], fp/fp[0], [0.]])
+#     results[mtype] = -np.trapz(1.-fpr, tpr)
+#
+#     # AUIN
+#     mtype = 'AUIN'
+#     denom = tp+fp
+#     denom[denom == 0.] = -1.
+#     pin_ind = np.concatenate([[True], denom > 0., [True]])
+#     pin = np.concatenate([[.5], tp/denom, [0.]])
+#     results[mtype] = -np.trapz(pin[pin_ind], tpr[pin_ind])
+#
+#     # AUOUT
+#     mtype = 'AUOUT'
+#     denom = tp[0]-tp+fp[0]-fp
+#     denom[denom == 0.] = -1.
+#     pout_ind = np.concatenate([[True], denom > 0., [True]])
+#     pout = np.concatenate([[0.], (fp[0]-fp)/denom, [.5]])
+#     results[mtype] = np.trapz(pout[pout_ind], 1.-fpr[pout_ind])
+#
+#     return results['AUROC'], results['AUIN'], results['AUOUT'], results['FPR']
+#
+# def get_curve(known, novel, method=None):
+#     tp, fp = dict(), dict()
+#     fpr_at_tpr95 = dict()
+#
+#     known.sort()
+#     novel.sort()
+#
+#     end = np.max([np.max(known), np.max(novel)])
+#     start = np.min([np.min(known),np.min(novel)])
+#
+#     all = np.concatenate((known, novel))
+#     all.sort()
+#
+#     num_k = known.shape[0]
+#     num_n = novel.shape[0]
+#
+#     if method == 'row':
+#         threshold = -0.5
+#     else:
+#         threshold = known[round(0.05 * num_k)]
+#
+#     tp = -np.ones([num_k+num_n+1], dtype=int)
+#     fp = -np.ones([num_k+num_n+1], dtype=int)
+#     tp[0], fp[0] = num_k, num_n
+#     k, n = 0, 0
+#     for l in range(num_k+num_n):
+#         if k == num_k:
+#             tp[l+1:] = tp[l]
+#             fp[l+1:] = np.arange(fp[l]-1, -1, -1)
+#             break
+#         elif n == num_n:
+#             tp[l+1:] = np.arange(tp[l]-1, -1, -1)
+#             fp[l+1:] = fp[l]
+#             break
+#         else:
+#             if novel[n] < known[k]:
+#                 n += 1
+#                 tp[l+1] = tp[l]
+#                 fp[l+1] = fp[l] - 1
+#             else:
+#                 k += 1
+#                 tp[l+1] = tp[l] - 1
+#                 fp[l+1] = fp[l]
+#
+#     j = num_k+num_n-1
+#     for l in range(num_k+num_n-1):
+#         if all[j] == all[j-1]:
+#             tp[j] = tp[j+1]
+#             fp[j] = fp[j+1]
+#         j -= 1
+#
+#     fpr_at_tpr95 = np.sum(novel > threshold) / float(num_n)
+#
+#     return tp, fp, fpr_at_tpr95
+#
+# def evaluate_all(in_scores, out_scores):
+#     in_examples = in_scores.reshape((-1,))
+#     out_examples = out_scores.reshape((-1,))
+#     auroc, aupr_in, aupr_out, fpr95 = cal_metric(in_examples, out_examples)
+#     return auroc, aupr_in, aupr_out, fpr95
