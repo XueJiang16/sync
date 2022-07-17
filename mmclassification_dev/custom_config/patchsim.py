9be1f7511ea1699312300f1da1e4b58978d30083
1@whughw.com <ghw970403>
2022-07-18 00:28:31 +0800

    ghw commit

--- mmclassification_dev/custom_config/patchsim.py
+++ mmclassification_dev/custom_config/patchsim.py
@@ -1,7 +1,7 @@
 method_name = 'FeatureMapSim'
 model_name = 'resnet50'
 train_dataset = 'LT_a8'
-custom_name = 'MSP'
+custom_name = 'Energy'
 if custom_name is not None:
     readable_name = '{}_{}_{}_{}'.format(method_name, model_name, train_dataset, custom_name)
 else:
