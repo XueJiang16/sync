e8f5d8500fb1e184de59cefcd216a616fadc1766
1@whughw.com <ghw970403>
2022-07-18 21:24:06 +0800

    ghw commit

--- mmclassification_dev/custom_config/patchsim.py
+++ mmclassification_dev/custom_config/patchsim.py
@@ -1,7 +1,7 @@
 method_name = 'FeatureMapSim'
 model_name = 'resnet50'
 train_dataset = 'Balance'
-custom_name = 'GradNormBatch'
+custom_name = 'Energy'
 if custom_name is not None:
     readable_name = '{}_{}_{}_{}'.format(method_name, model_name, train_dataset, custom_name)
 else:
@@ -15,7 +15,7 @@ model = dict(
     order = 1,
     mode = 'std',
     ood_detector = dict(
-        type='GradNormBatch',
+        type='Energy',
         debug_mode=False,
         num_classes=1000,
         # temperature=1,
