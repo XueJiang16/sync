0676296ddbb9de74e6f17c1ca5391badef08d857
jx <763565607@qq.com>
2022-07-18 21:41:54 +0800

    jx commit

---
+++ mmclassification_dev/custom_config/inat_ib/resnet101.py
@@ -0,0 +1,75 @@
+method_name = 'MSP'
+model_name = 'resnet101'
+train_dataset = 'inat'
+custom_name = None
+if custom_name is not None:
+    readable_name = '{}_{}_{}_{}'.format(method_name, model_name, train_dataset, custom_name)
+else:
+    readable_name ='{}_{}_{}'.format(method_name, model_name, train_dataset)
+quick_test = False
+training_file = None
+model = dict(
+    type=method_name,
+    num_classes=8042,
+    # temperature=1,
+    target_file=training_file,
+    classifier=dict(
+        type='ImageClassifier',
+        init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/ood_ckpt/ckpt/inat/epoch_100.pth'),
+        backbone=dict(
+            type='ResNet',
+            depth=101,
+            num_stages=4,
+            out_indices=(3,),
+            style='pytorch'),
+        neck=dict(type='GlobalAveragePooling'),
+        head=dict(
+            type='LinearClsHead',
+            num_classes=8042,
+            in_channels=2048,
+            loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+            topk=(1, 5))
+    )
+)
+# pipline =[dict(type='Collect', keys=['img'])]
+pipline =[dict(type='Collect', keys=['img', 'type'])]
+data = dict(
+    samples_per_gpu=256,
+    workers_per_gpu=4,
+    id_data=dict(
+        name='iNaturalist',
+        type='JsonDataset',
+        path='/data/csxjiang/',
+        data_ann='/data/csxjiang/ood_data/inat/val2018.json',
+        pipeline=pipline,
+        len_limit=5000 if quick_test else -1,
+    ),
+    ood_data=[
+        dict(
+            name='SUN',
+            type='FolderDataset',
+            path='/data/csxjiang/ood_data/SUN/images',
+            pipeline=pipline,
+            len_limit=1000 if quick_test else -1,
+        ),
+        dict(
+            name='Places',
+            type='FolderDataset',
+            path='/data/csxjiang/ood_data/Places/images',
+            pipeline=pipline,
+            len_limit=1000 if quick_test else -1,
+        ),
+        dict(
+            name='Textures',
+            type='FolderDataset',
+            path='/data/csxjiang/ood_data/Textures/dtd/images_collate',
+            pipeline=pipline,
+            len_limit=1000 if quick_test else -1,
+        ),
+    ],
+
+)
+dist_params = dict(backend='nccl')
+log_level = 'CRITICAL'
+# log_level = 'INFO'
+work_dir = './results/inat'
