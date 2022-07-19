1a82595da64b358ac826b7f087779a250da884c6
1@whughw.com <ghw970403>
2022-07-19 21:59:21 +0800

    ghw commit

--- mmclassification_dev/mmcls/models/ood/feature_level.py
+++ mmclassification_dev/mmcls/models/ood/feature_level.py
@@ -132,10 +132,10 @@ class FeatureMapSim(BaseModule):
                 patch_sim = torch.abs(feature_crops - patch_mean).mean(dim=(-1, -2))  # for ID: .mean(dim=-2)
             elif self.mode == 'fuse_mean':
                 feature_crops = feature_c5.flatten(2)
-                channel_mean = feature_crops.mean(1).unsqueeze(1)  # (N, C, H*W) -> (N, C)
+                channel_mean = feature_crops.mean(1).unsqueeze(1)  # (N, C, H*W) -> (N, 1, H*W)
                 channel_sim = torch.abs(feature_crops - channel_mean).mean(dim=(-1, -2))
-                spatial_mean = feature_crops.mean(-1).unsqueeze(-1)  # (N, C, H*W) -> (N, C)
-                spatial_sim = torch.abs(feature_crops - spatial_mean).mean(dim=(-1, -2))  # for ID: .mean(dim=-2)
+                spatial_mean = feature_crops.mean(-1).unsqueeze(-1)  # (N, C, H*W) -> (N, C, 1)
+                spatial_sim = torch.abs(feature_crops - spatial_mean).mean(dim=(-1, -2))
                 patch_sim = channel_sim + spatial_sim
             elif self.mode == 'extract_feature_sim':
                 # (N, C, H*W) -> (N, C) -> (C,) -> argsort -> topK_idx -> id_ood_inference -> feature_crops[:, topK_idx]
@@ -143,11 +143,11 @@ class FeatureMapSim(BaseModule):
                 patch_mean = feature_crops.mean(-1).unsqueeze(-1)  # (N, C, H*W) -> (N, C, 1)
                 patch_sim = torch.abs(feature_crops - patch_mean).mean(dim=(0, 2))  # for ID: .mean(dim=(0, 2))
             ood_scores = patch_sim
-        # if self.has_ood_detector:
-        #     ood_scores, _ = self.ood_detector(**input)
-        #     patch_sim = ((1 / self.threshold) ** (self.order)) * torch.pow(patch_sim, self.order)
-        #     patch_sim[patch_sim > 1] = 1
-        #     ood_scores *= patch_sim
-        # else:
-        #     ood_scores = patch_sim
+        if self.has_ood_detector:
+            ood_scores, _ = self.ood_detector(**input)
+            patch_sim = ((1 / self.threshold) ** (self.order)) * torch.pow(patch_sim, self.order)
+            patch_sim[patch_sim > 1] = 1
+            ood_scores *= patch_sim
+        else:
+            ood_scores = patch_sim
         return ood_scores, type
