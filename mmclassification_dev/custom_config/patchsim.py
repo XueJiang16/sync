577cdde59a3c4038fa216838dcb2111edebaad30
1@whughw.com <ghw970403>
2022-07-19 23:15:44 +0800

    resnet101 spatial mean

--- mmclassification_dev/custom_config/patchsim.py
+++ mmclassification_dev/custom_config/patchsim.py
@@ -1,7 +1,7 @@
 method_name = 'FeatureMapSim'
 model_name = 'resnet50'
 train_dataset = 'Balance'
-custom_name = 'Energy'
+custom_name = 'GradNormBatch'
 if custom_name is not None:
     readable_name = '{}_{}_{}_{}'.format(method_name, model_name, train_dataset, custom_name)
 else:
@@ -15,7 +15,7 @@ model = dict(
     order = 1,
     mode = 'mean',
     ood_detector = dict(
-        type='Energy',
+        type='GradNormBatch',
         debug_mode=False,
         num_classes=1000,
         # temperature=1,
