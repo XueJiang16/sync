b741a75b5eee65c0acf840abb77662bad941919e
1@whughw.com <ghw970403>
2022-07-21 02:04:00 +0800

    ghw commit

--- mmclassification_dev/mmcls/models/backbones/resnet.py
+++ mmclassification_dev/mmcls/models/backbones/resnet.py
@@ -433,7 +433,8 @@ class RandomBlock(BaseModule):
         #     out = _inner_forward(x)
         #
         # out = self.relu(out)
-        out = self.relu(x + (torch.rand_like(x) - 0.5) / 25)
+        k = 20
+        out = self.relu(x + (torch.rand_like(x) - 0.5) / k)  # (torch.rand_like(x) - 0.5) ~ U[-0.5, 0.5)
 
         return out
 
