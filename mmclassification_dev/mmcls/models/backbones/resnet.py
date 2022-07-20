2ed8d3bd778384c66e440192f829ed211e8c8101
1@whughw.com <ghw970403>
2022-07-20 23:40:29 +0800

    ghw commit

--- mmclassification_dev/mmcls/models/backbones/resnet.py
+++ mmclassification_dev/mmcls/models/backbones/resnet.py
@@ -634,7 +634,7 @@ class ResNet(BaseBackbone):
                  with_cp=False,
                  zero_init_residual=True,
                  init_cfg=[
-                     dict(type='Uniform', layer=['Conv2d']),
+                     dict(type='Constant', layer=['Conv2d'], val=0.5),
                      # dict(type='Kaiming', layer=['Conv2d'], mode='fan_in'),
                      dict(
                          type='Constant',
