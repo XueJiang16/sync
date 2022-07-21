4ef202205a659db00c1073147f38397df4a9e188
1@whughw.com <ghw970403>
2022-07-21 22:54:11 +0800

    normal distribution

--- mmclassification_dev/custom_config/patchsim.py
+++ mmclassification_dev/custom_config/patchsim.py
@@ -35,7 +35,7 @@ model = dict(
                 out_indices=(3,),
                 style='pytorch',
                 random_block=1,
-                random_block_k=12,
+                random_block_k=20,
             ),
             neck=dict(type='GlobalAveragePooling'),
             head=dict(
