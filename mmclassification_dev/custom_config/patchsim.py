eecd3abcadcd234ad619e774883f1266bf9d5e3c
1@whughw.com <ghw970403>
2022-07-21 11:56:50 +0800

    Simplify random block

--- mmclassification_dev/custom_config/patchsim.py
+++ mmclassification_dev/custom_config/patchsim.py
@@ -34,8 +34,8 @@ model = dict(
                 num_stages=4,
                 out_indices=(3,),
                 style='pytorch',
-                random_block=17,
-                random_block_k=20,
+                random_block=0,
+                random_block_k=2.5,
             ),
             neck=dict(type='GlobalAveragePooling'),
             head=dict(
