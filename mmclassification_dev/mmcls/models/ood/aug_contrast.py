4c115fb1b0800329cc306bfa941b56b9ffe8d942
1@whughw.com <ghw970403>
2022-07-22 16:20:23 +0800

    [New Feature] aug contrast

--- mmclassification_dev/mmcls/models/ood/aug_contrast.py
+++ mmclassification_dev/mmcls/models/ood/aug_contrast.py
@@ -22,8 +22,11 @@ class AugContrast(BaseModule):
             type = input['type']
             del input['type']
         with torch.no_grad():
-            outputs_orig = self.classifier(return_loss=False, softmax=True, post_process=False, **input)
-            input["img"] = input["img"] + (torch.rand_like(input["img"])-0.5) / 3
-            outputs_aug = self.classifier(return_loss=False, softmax=True, post_process=False, **input)
-            confs = -torch.abs(outputs_orig - outputs_aug).sum(-1)
+            outputs_orig, features_orig = self.classifier(return_loss=False, softmax=True, post_process=False,
+                                                          require_features=True, **input)
+            input["img"] = input["img"] + (torch.rand_like(input["img"])-0.5) / 4
+            outputs_aug, features_aug = self.classifier(return_loss=True, softmax=True, post_process=False,
+                                                        require_features=False, **input)
+            confs = -torch.abs(features_orig - features_aug).sum(-1)
+            # confs = -torch.abs(outputs_orig - outputs_aug).sum(-1)
         return confs, type
